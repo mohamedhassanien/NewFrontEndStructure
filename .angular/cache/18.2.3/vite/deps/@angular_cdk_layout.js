ba9b753c5875ef14b35dd735e7f368a7734b55bc
Ahmed Ramadan <ahmedramadan.net@gmail.com>
2024-09-26 14:22:33 +0300

    default scss file

---
+++ .angular/cache/18.2.3/vite/deps/@angular_cdk_layout.js
@@ -0,0 +1,15 @@
+import {
+  BreakpointObserver,
+  Breakpoints,
+  LayoutModule,
+  MediaMatcher
+} from "./chunk-VNPA57D2.js";
+import "./chunk-CQGX6PHB.js";
+import "./chunk-YY4IBSK4.js";
+export {
+  BreakpointObserver,
+  Breakpoints,
+  LayoutModule,
+  MediaMatcher
+};
+//# sourceMappingURL=@angular_cdk_layout.js.map
