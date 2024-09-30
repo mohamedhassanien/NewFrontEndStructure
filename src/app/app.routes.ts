d9184fa3e560b7179cad475a564118b578dad60a
hagerhesham11 <hagerhesham72@gmail.com>
2024-09-30 11:55:46 +0300

    creating forgot password comp ui

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -5,6 +5,9 @@ import { StatisticsComponent } from './components/pages/statistics/statistics.co
 import { ProspectComponent } from './components/pages/prospect/prospect.component';
 import { authGuard } from './guard/auth.guard';
 import { ResetPasswordComponent } from './components/auth/reset-password/reset-password.component';
+import { RegisterComponent } from './components/auth/register/register.component';
+import { VerfiyComponent } from './components/auth/verfiy/verfiy.component';
+import { ForgotPasswordComponent } from './components/auth/forgot-password/forgot-password.component';
 
 export const routes: Routes = [
     {
@@ -19,6 +22,18 @@ export const routes: Routes = [
     {
         path:'reset-password',
         component:ResetPasswordComponent
+    },
+    {
+        path:'sign-up',
+        component:RegisterComponent
+    },
+    {
+        path:'verify',
+        component:VerfiyComponent
+    },
+    {
+        path:'forgot-password',
+        component:ForgotPasswordComponent
     },
     {
         path: '',
