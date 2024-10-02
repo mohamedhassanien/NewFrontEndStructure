18f0972942f65bce8af3ebd1d849d14fbcd13959
hagerhesham11 <hagerhesham72@gmail.com>
2024-10-02 17:50:35 +0300

    creating login form

--- src/app/components/auth/login/login.component.ts
+++ src/app/components/auth/login/login.component.ts
@@ -1,6 +1,6 @@
-import { Component, inject } from '@angular/core';
+import { Component, inject, OnInit } from '@angular/core';
 import { RouterModule } from '@angular/router';
-import { FormsModule } from '@angular/forms';
+import { FormControl, FormGroup, FormsModule, ReactiveFormsModule, Validators } from '@angular/forms';
 import { Login } from '../../../models/login.model';
 import { LoginService } from '../../../services/auth/login.service';
 import { CommonModule } from '@angular/common';
@@ -10,16 +10,24 @@ import { MatIconModule } from '@angular/material/icon';
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [FormsModule, RouterModule ,CommonModule , MatIconModule],
+  imports: [FormsModule, RouterModule ,CommonModule , MatIconModule ,ReactiveFormsModule],
   templateUrl: './login.component.html',
   styleUrl: './login.component.scss'
 })
-export class LoginComponent {
+export class LoginComponent implements OnInit{
   showPassword:boolean = false ;
+  loginForm!:FormGroup;
 
    loginObj:Login = {
      email: '',
      password: ''
+   }
+
+   ngOnInit(): void {
+     this.loginForm = new FormGroup({
+      email: new FormControl('',[Validators.required,Validators.email]),
+      password:new FormControl('',[Validators.required])
+     })
    }
 
    loginServ = inject(LoginService)
@@ -33,5 +41,10 @@ export class LoginComponent {
 
    togglePassswordVisiblity():void{
     this.showPassword = !this.showPassword;
+   }
+
+   onSubmit(loginForm:FormGroup){
+    this.loginObj = loginForm.value;
+    console.log(this.loginObj)
    }
 }
