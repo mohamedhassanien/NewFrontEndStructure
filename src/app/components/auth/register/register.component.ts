e6d1965e61ce3d12c26d3c4f9f5b59d185d31be6
hagerhesham11 <hagerhesham72@gmail.com>
2024-10-03 16:46:47 +0300

    creating register form and login form

--- src/app/components/auth/register/register.component.ts
+++ src/app/components/auth/register/register.component.ts
@@ -1,18 +1,47 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, FormsModule, ReactiveFormsModule, Validators } from '@angular/forms';
 import { RouterModule } from '@angular/router';
+import { Register } from '../../../models/register.model';
 
 @Component({
   selector: 'app-register',
   standalone: true,
   imports: [
     RouterModule,
-    
+    FormsModule,
+    ReactiveFormsModule
   ],
   
   templateUrl: './register.component.html',
   styleUrl: './register.component.scss'
 })
-export class RegisterComponent {
+export class RegisterComponent implements OnInit {
+registerForm!:FormGroup;
+registerObj:Register ={
+  firstName:'',
+  lastName:'',
+  phoneNumber:'',
+  email:'',
+  country:'',
+  age:'',
+  password:'',
+  rePassword:''
+}
+ngOnInit(): void {
+  this.registerForm = new FormGroup({
+    firstName: new FormControl('',[Validators.required,Validators.pattern('^[a-zA-Z]+$')]),
+    lastName: new FormControl('',[Validators.required,Validators.pattern('^[a-zA-Z]+$')]),
+    phoneNumber: new FormControl('',[Validators.required,Validators.pattern('[0-9]')]),
+    email:new FormControl('',[Validators.required,Validators.email]),
+    country:new FormControl('',[Validators.required]),
+    age:new FormControl('',[Validators.required]),
+    password:new FormControl('',[Validators.pattern('[a-zA-Z0-9]{8,16}')])
+  })
+}
 
 
+onSubmit(registerForm:FormGroup){
+  this.registerObj = registerForm.value;
+  console.log(this.registerObj)
+}
 }
