f3dedb5902178f8a41b4167ca7ba3f94e661258a
hagerhesham11 <hagerhesham72@gmail.com>
2024-10-03 18:12:54 +0300

    adding password and repassword eye for reset password component

--- src/app/components/auth/reset-password/reset-password.component.ts
+++ src/app/components/auth/reset-password/reset-password.component.ts
@@ -1,13 +1,25 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, OnInit } from '@angular/core';
 import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-reset-password',
   standalone: true,
-  imports: [RouterModule],
+  imports: [RouterModule , CommonModule],
   templateUrl: './reset-password.component.html',
   styleUrl: './reset-password.component.scss'
 })
-export class ResetPasswordComponent {
+export class ResetPasswordComponent implements OnInit{
+ showPassword:boolean = false;
+ showRepass:boolean = false; 
+ngOnInit(): void {
+  
+}
 
+  togglePasswordVisiblity():void{
+    this.showPassword = !this.showPassword;
+  }
+  toggleRepassVisiblity():void{
+    this.showRepass = !this.showRepass;
+  }
 }
