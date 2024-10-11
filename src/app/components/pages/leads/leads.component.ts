19a612b81861dcdd59246b73dda109b55ff86deb
Ahmed Ramadan <ahmedramadan.net@gmail.com>
2024-10-11 16:46:09 +0300

    fixed leads table iossu and make the row menu

--- src/app/components/pages/leads/leads.component.ts
+++ src/app/components/pages/leads/leads.component.ts
@@ -1,18 +1,52 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { Router } from '@angular/router';
+
 import { MainActionsComponent } from "../../layout/main-actions/main-actions.component";
 import { MainFilterComponent } from "../../layout/main-filter/main-filter.component";
 
+
 import { Leads } from '../../../models/leads';
 import { LeadsService } from '../../../services/components/leads.service';
-import { TableModule } from 'primeng/table';
-import { CommonModule } from '@angular/common';
 
-import { FormsModule } from '@angular/forms';
+
+
+
+import { ProgressBarModule } from 'primeng/progressbar';
+import { DropdownModule } from 'primeng/dropdown';
+import { TableModule } from 'primeng/table';
+
+
+import { MatMenuModule } from '@angular/material/menu';
+import { MatButtonModule } from '@angular/material/button';
+
+import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
+
+import { NgxIntlTelInputModule } from 'ngx-intl-tel-input';
+
+
+
+
+import {
+  SearchCountryField,
+  CountryISO
+} from "ngx-intl-tel-input";
 
 @Component({
   selector: 'app-leads',
   standalone: true,
-  imports: [MainActionsComponent, MainFilterComponent, TableModule, CommonModule, FormsModule],
+  imports: [MainActionsComponent,
+    MainFilterComponent,
+    TableModule,
+    CommonModule,
+    FormsModule,
+    ProgressBarModule,
+    DropdownModule,
+    ReactiveFormsModule,
+    MatMenuModule,
+    MatButtonModule,
+    NgxIntlTelInputModule],
   templateUrl: './leads.component.html',
   styleUrl: './leads.component.scss',
   providers: [LeadsService]
@@ -20,17 +54,91 @@ import { FormsModule } from '@angular/forms';
 })
 export class LeadsComponent {
   btn_title = 'Add Lead'
-
   leads!: Leads[];
+  scores: number[] = [1, 2, 3, 4, 5];
+  employees: any[] = [{
+    name: "Ali",
+  },
+  {
+    name: "Nicolas",
+  }, {
+    name: "Salah",
+  },
+  {
+    name: "Cyrus Alborzi",
+  },
+  {
+    name: "Ei Mya HMUE KHIN",
+  },
+  {
+    name: "Fatima Zaari",
+  },
+  {
+    name: "Hasnae srayi",
+  },
+  {
+    name: "Houda Berrada",
+  }
+  ];
 
-  constructor(private leadsService: LeadsService) { }
+  separateDialCode = false;
+  SearchCountryField = SearchCountryField;
+  CountryISO = CountryISO;
+  preferredCountries: CountryISO[] = [
+    CountryISO.UnitedStates,
+    CountryISO.UnitedKingdom
+  ];
+
+
+  constructor(private leadsService: LeadsService, private _Router: Router, private _ModalService: NgbModal,) {
+
+  }
+
 
 
   ngOnInit() {
-    this.leadsService.getProductsMini().then((data:any) => {
+    this.leadsService.getProductsMini().then((data: any) => {
       this.leads = data;
     });
   }
+  changePreferredCountries() {
+    this.preferredCountries = [CountryISO.India, CountryISO.Canada];
+  }
+
+  getProgressColor(days: number): string {
+    if (days <= 5) return 'progress-green';
+    else if (days <= 10) return 'progress-yellow';
+    else if (days <= 20) return 'progress-orange';
+    else if (days <= 25) return 'progress-red';
+    return 'progress-red';
+  }
+
+  setProgressValue(value: number) {
+    let max = 25;
+    if (value > max) {
+      value = 25;
+    }
+    return (value / max) * 100;
+  }
+
+
+  openModal(element: any, className: string, size: string, data: any[]) {
+    this._ModalService.open(element, { windowClass: className, size: size });
+  }
+
+  sendToMe(name: any) {
+
+  }
+  assignToEmployee(leadName: any, empName: any) {
+
+  }
+  sendToProspect(leadsName:any){
+
+  }
+  openModalArchive(leadsName:any){
+
+  }
+  
 }
 
 
--- src/app/services/components/leads.service.ts
+++ src/app/services/components/leads.service.ts
@@ -1449,16 +1449,4 @@ export class LeadsService {
     getProductsSmall() {
         return Promise.resolve(this.getProductsData().slice(0, 10));
     }
-
-    getProducts() {
-        return Promise.resolve(this.getProductsData());
-    }
-
-    getProductsWithOrdersSmall() {
-        return Promise.resolve(this.getProductsWithOrdersData().slice(0, 10));
-    }
-
-    getProductsWithOrders() {
-        return Promise.resolve(this.getProductsWithOrdersData());
-    }
 };
